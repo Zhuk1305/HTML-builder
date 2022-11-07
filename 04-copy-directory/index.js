e6ab7b12cfb2ff25be22719f049ffc120606cc5b
Zhuk1305 <vaneczhuk@gmail.com>
2022-11-08 00:43:34 +0300

    feat: add code for 4-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+const fs = require("fs");
+const path = require("path");
+fs.rm(
+  path.join(__dirname, "files-copy"),
+  { force: true, recursive: true },
+  (err) => {
+    if (err) throw err;
 
+    fs.mkdir(path.join(__dirname, "files-copy"), { recursive: true }, (err) => {
+      if (err) throw err;
+    });
+
+    fs.readdir(
+      path.join(__dirname, "files"),
+      { withFileTypes: true },
+      (err, data) => {
+        if (err) throw err;
+        for (const file of data) {
+          if (file.isFile()) {
+            copyFile(file.name);
+          }
+        }
+      }
+    );
+  }
+);
+
+function copyFile(file) {
+  fs.copyFile(
+    path.join(__dirname, "files", file),
+    path.join(__dirname, "files-copy", file),
+    (err) => {
+      if (err) throw err;
+    }
+  );
+}
