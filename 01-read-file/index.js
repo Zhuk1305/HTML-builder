48c25e6474a6558e209340434a919b4df5276b3e
Zhuk1305 <vaneczhuk@gmail.com>
2022-11-07 14:53:34 +0300

    feat: add code for 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const { stdout } = process;
+
+const fs = require("fs");
+const path = require("path");
+
+const readableStream = fs.createReadStream(
+  path.join(__dirname, "text.txt"),
+  "utf-8"
+);
+readableStream.on("data", (data) => stdout.write(data));
